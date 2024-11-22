30dbbb83cfaa46f734ff8a7b2a67567f10523b67
Laynaria <pro.laynaria@outlook.fr>
2024-11-22 17:46:10 +0100

    Following supabase documentation to re-initialize repository with next and supabase installed and configured, also tried to query that db.

---
+++ app/countries/page.tsx
@@ -0,0 +1,17 @@
+import { createClient } from "@/utils/supabase/server";
+
+export default async function Countries() {
+  const supabase = await createClient();
+  const { data: countries } = await supabase.from("countries").select();
+
+  return (
+    <>
+      <pre>{JSON.stringify(countries, null, 2)}</pre>
+      <section>
+        {countries?.map((country) => (
+          <article key={country.id}>{country.name}</article>
+        ))}
+      </section>
+    </>
+  );
+}
